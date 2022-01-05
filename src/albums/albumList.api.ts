b5087e10ed33f19183c9ccd3cc230752c4e24f95
Gintautas Vasauskas <giintasv9@gmail.com>
2022-01-05 22:14:55 +0200

    added missing Album import

--- src/albums/albumList.api.ts
+++ src/albums/albumList.api.ts
@@ -1,4 +1,5 @@
 import axiosInstance from '../axios';
+import { Album } from './albumList.model';
 
 export const getUserAlbums = async (userId: number): Promise<Album[]> => {
   const { data } = await axiosInstance.get(`/users/${userId}/albums`);
